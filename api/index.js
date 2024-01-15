ff076469ad261517b836351ea6beb1bf341e0cbf
Yaroslav <yaroslavv86@gmail.com>
2024-01-15 23:35:22 +0600

    fix

--- api/index.js
+++ api/index.js
@@ -15,19 +15,19 @@ app.use(passport.initialize());
 const jwt = require("jsonwebtoken");
 
 mongoose
-   .connect(
-      "mongodb+srv://yaroslavv86:I3CBF27VeTTI32ee@cluster0.scwd76n.mongodb.net/",
-      { useNewUrlParser: true, useUnifiedTopology: true }
-   )
-   .then(() => {
-      console.log("connected to Mongo DB");
-   })
-   .catch((err) => {
-      console.log("Error connecting to MongoDB", err);
-   });
+  .connect(
+    "mongodb+srv://yaroslavv86:I3CBF27VeTTI32ee@cluster0.scwd76n.mongodb.net/",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    console.log("connected to Mongo DB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
 
 app.listen(port, () => {
-   console.log("Server running on port", port);
+  console.log("Server running on port", port);
 });
 
 const User = require("./models/user");
@@ -35,115 +35,115 @@ const Message = require("./models/message");
 
 //function to create a token for the user
 const createToken = (userId) => {
-   const payload = {
-      userId: userId,
-   };
+  const payload = {
+    userId: userId,
+  };
 
-   //generate the token with a secret key and expiration time
-   const token = jwt.sign(payload, "Q$r2K6W8n!jCW%Zk", { expiresIn: "1h" });
-   return token;
+  //generate the token with a secret key and expiration time
+  const token = jwt.sign(payload, "Q$r2K6W8n!jCW%Zk", { expiresIn: "1h" });
+  return token;
 };
 
 //endpoint for registration of the user
 
 app.post("/register", (req, res) => {
-   const { name, email, password, image } = req.body;
+  const { name, email, password, image } = req.body;
 
-   //create a new User object
-   const newUser = new User({ name, email, password, image });
+  //create a new User object
+  const newUser = new User({ name, email, password, image });
 
-   //save the user to the database
+  //save the user to the database
 
-   newUser
-      .save()
-      .then(() => {
-         res.status(200).json({ message: "User registered successfully" });
-      })
-      .catch((err) => {
-         console.log("Error registrating user", err);
-         res.status(500).json({ message: "Error registering the user!" });
-      });
+  newUser
+    .save()
+    .then(() => {
+      res.status(200).json({ message: "User registered successfully" });
+    })
+    .catch((err) => {
+      console.log("Error registrating user", err);
+      res.status(500).json({ message: "Error registering the user!" });
+    });
 });
 
 //endpoint for loggin in
 
 app.post("/login", (req, res) => {
-   const { email, password } = req.body;
+  const { email, password } = req.body;
 
-   if (!email || !password) {
-      return res
-         .status(404)
-         .json({ message: "Email and the password are required" });
-   }
+  if (!email || !password) {
+    return res
+      .status(404)
+      .json({ message: "Email and the password are required" });
+  }
 
-   const user = User.findOne({ email })
-      .then((user) => {
-         if (!user) {
-            //user not found
-            return res.status(404).json({ message: "User not found" });
-         }
+  const user = User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        //user not found
+        return res.status(404).json({ message: "User not found" });
+      }
 
-         if (user.password !== password) {
-            return res.status(404).json({ message: "Invalid Password" });
-         }
+      if (user.password !== password) {
+        return res.status(404).json({ message: "Invalid Password" });
+      }
 
-         const token = createToken(user._id);
-         res.status(200).json({ token });
-      })
-      .catch((error) => {
-         console.log("error in finding the user", error);
-         res.status(500).json({ message: "Internal server Error" });
-      });
+      const token = createToken(user._id);
+      res.status(200).json({ token });
+    })
+    .catch((error) => {
+      console.log("error in finding the user", error);
+      res.status(500).json({ message: "Internal server Error" });
+    });
 });
 
 //endpoint to access all the users except the user who's is currently logged in
 app.get("/users/:userId", (req, res) => {
-   const loggedInUserId = req.params.userId;
-   User.find({ _id: { $ne: loggedInUserId } })
-      .then((users) => {
-         res.status(200).json(users);
-      })
-      .catch((error) => {
-         console.log(error);
-         res.status(500).json({ message: "There is an error" });
-      });
+  const loggedInUserId = req.params.userId;
+  User.find({ _id: { $ne: loggedInUserId } })
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ message: "There is an error" });
+    });
 });
 
 //endpoint to send a request to a user
 app.post("/friend-request", async (req, res) => {
-   const { currentUserId, selectedUserId } = req.body;
+  const { currentUserId, selectedUserId } = req.body;
 
-   try {
-      //update the recepient's friendRequestArray
-      await User.findByIdAndUpdate(selectedUserId, {
-         $push: { friendsRequests: currentUserId },
-      });
+  try {
+    //update the recepient's friendRequestArray
+    await User.findByIdAndUpdate(selectedUserId, {
+      $push: { friendsRequests: currentUserId },
+    });
 
-      //update the sender's sentFriendRequests array
-      await User.findByIdAndUpdate(currentUserId, {
-         $push: { sentFriendsRequests: selectedUserId },
-      });
+    //update the sender's sentFriendRequests array
+    await User.findByIdAndUpdate(currentUserId, {
+      $push: { sentFriendsRequests: selectedUserId },
+    });
 
-      res.sendStatus(200);
-   } catch (error) {
-      res.sendStatus(500);
-   }
+    res.sendStatus(200);
+  } catch (error) {
+    res.sendStatus(500);
+  }
 });
 
 //endpoint to show all the friend-requests of a particular user
 app.get("/requestedFriends/:userId", async (req, res) => {
-   const userId = req.params.userId;
+  const userId = req.params.userId;
 
-   //fetch the user document based on the user id
+  //fetch the user document based on the user id
 
-   User.findById(userId)
-      .populate("friendsRequests", "name email image")
-      .lean()
-      .then((data) => {
-         res.status(200).json(data);
-      })
-      .catch((error) => {
-         console.log(error);
-         res.status(500).json({ message: "There is an error" });
-      });
+  User.findById(userId)
+    .populate("friendsRequests", "name email image")
+    .lean()
+    .then((data) => {
+      res.status(200).json(data.friendsRequests);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ message: "There is an error" });
+    });
 });
--- screens/FriendsScreen.js
+++ screens/FriendsScreen.js
@@ -4,27 +4,38 @@ import { UserType } from "../UserContext";
 import axios from "axios";
 
 const FriendsScreen = () => {
-   const { userId, setUserId } = useContext(UserType);
-   const [requests, setRequests] = useState([]);
+  const { userId, setUserId } = useContext(UserType);
+  const [requests, setRequests] = useState([]);
 
-   useEffect(() => {
-      axios
-         .get(`http://localhost:8000/requestedFriends/${userId}`)
-         .then((response) => {
-            setRequests(response.data);
-         })
-         .catch((error) => {
-            console.log("error retrieving users", error);
-         });
-   }, []);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8000/requestedFriends/${userId}`)
+      .then((response) => {
+        if (response.status === 200) {
+          console.log(response);
+          const friendRequestData = response.data.map((item) => {
+            return {
+              _id: item.id,
+              name: item.name,
+              email: item.email,
+              image: item.image,
+            };
+          });
+          setRequests(friendRequestData);
+        }
+      })
+      .catch((error) => {
+        console.log("error retrieving users", error);
+      });
+  }, []);
 
-   console.log(requests);
+  console.log(requests);
 
-   return (
-      <View>
-         <Text>FriendsScreen</Text>
-      </View>
-   );
+  return (
+    <View>
+      <Text>FriendsScreen</Text>
+    </View>
+  );
 };
 
 export default FriendsScreen;
